982b5f1f4b8b2e9683778339a94bcab6b1bce3db
Alexandru Maracine <alexandru.maracine@endava.com>
2026-01-08 14:55:37 +0200

    cloud backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-skycastnow-tfstate"
-    storage_account_name = "tfstateskycastnow19471"
-    container_name       = "tfstate"
-    key                  = "skycastnow.terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
